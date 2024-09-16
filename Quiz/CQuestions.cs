b1147cf7734b34b89d5bf914533a50280c1f0658
David Eriksson <david.eriksson2@elev.ga.ntig.se>
2024-09-16 09:56:46 +0200

    löst?

--- Quiz/CQuestions.cs
+++ Quiz/CQuestions.cs
@@ -13,7 +13,7 @@ bool question2activ = false;
 public bool AddQuestions = true;
 public int ADQusetions = 0;
 public int AnsQuestions = 0;
-public int  intefall= 0;
+public int  intefall;
 public int QuestionPoints= 0;
 public int CorectQuestions =0;
 string question = "0";
@@ -100,76 +100,83 @@ public void ReadAnswer() {
 Answer = SC.ReadLine();
 }
 public void PromptAnswer() {
-SC.WriteLine("Answer");
 AnsQuestions ++;
-SC.WriteLine("PromptAnswer");
-
     switch ((string)(Answer)) {
     case (string)"Stanley Kubrick": {
-        SC.WriteLine("Correct answer, you recive 50 points");
+SC.WriteLine("Correct answer, you recive 50 points");
         QuestionPoints =+ 50;
+        intefall = 1;
         CorectQuestions++;
     break;    }
 
     case (string)"Top Gun": {
-        SC.WriteLine("Correct answer, you recive 100 points");
+SC.WriteLine("Correct answer, you recive 100 points");
         QuestionPoints =+ 100;
+        intefall = 1;
         CorectQuestions++;
     break;    }
 
     case (string)"Springfield": {
-        SC.WriteLine("Correct answer, you recive 150 points");
+SC.WriteLine("Correct answer, you recive 150 points");
         QuestionPoints =+ 150;
+        intefall = 1;
         CorectQuestions++;
     break;    }
     
     case (string)"Victoriasjön": {
-        SC.WriteLine("Correct answer, you recive 50 points");
+SC.WriteLine("Correct answer, you recive 50 points");
         QuestionPoints =+ 50;
+        intefall = 1;
         CorectQuestions++;
     break;    }
     case (string)"Vatikanstaten": {
-        SC.WriteLine("Correct answer, you recive 100 points");
+SC.WriteLine("Correct answer, you recive 100 points");
         QuestionPoints =+ 100;
+        intefall = 1;
         CorectQuestions++;
     break;    }
     case (string)"Canberra": {
-        SC.WriteLine("Correct answer, you recive 150 points");
+SC.WriteLine("Correct answer, you recive 150 points");
         QuestionPoints =+ 150;
+        intefall = 1;
         CorectQuestions++;
     break;    }
     case (string)"John Lennon": {
-        SC.WriteLine("Correct answer, you recive 50 points");
+SC.WriteLine("Correct answer, you recive 50 points");
         QuestionPoints =+ 50;
+        intefall = 1;
         CorectQuestions++;
     break;    }
     case (string)"Irland": {
-        SC.WriteLine("Correct answer, you recive 100 points");
+SC.WriteLine(@"Correct answer, you recive 100 points");
         QuestionPoints =+ 100;
+        intefall = 1;
         CorectQuestions++;
     break;    }
     case (string)"1977": {
-        SC.WriteLine("Correct answer, you recive 150 points");
+SC.WriteLine(@"Correct answer, you recive 150 points");
         QuestionPoints =+ 150;
+        intefall = 1;
         CorectQuestions++;
     break;    }
 }
 if (Question1Answer == Answer) {
     SC.WriteLine(Question1Answer);
-        SC.WriteLine("Correct answer, you recive what ever you want for answering YOUR own question");
-        intefall ++;
+SC.WriteLine("Correct answer, you recive what ever you want for answering YOUR own question");
         CorectQuestions++;
+        intefall = 1;
 }
 if (Question2Answer == Answer) {
     SC.WriteLine(Question2Answer);
-        SC.WriteLine("Correct answer, you recive what ever you want for answering YOUR own question");
-         intefall ++;
+SC.WriteLine("Correct answer, you recive what ever you want for answering YOUR own question");
          CorectQuestions++;
+         intefall = 1;
 }
-if ( intefall == 0) {
-SC.WriteLine("Wrong Answer, No points for you!"); 
+if (intefall == 2) {
+SC.WriteLine(@"
+Wrong Answer, No points for you!"); 
 }
-SC.ReadLine();  
+SC.ReadLine();
  intefall = 0;
  Answer = "";
 }
@@ -199,7 +206,7 @@ SC.WriteLine(@$"
     #  (12) {Question2Head}                                          
     #------------------------------------------------------- #
     ##########################################################");} 
-
+    question = SC.ReadLine();
  }
 
 
@@ -209,60 +216,71 @@ public void PromptQuestion () {
 switch ((string)(question)) {
     
     case (string)"1": {
+        intefall = 2;
         SC.Clear();
         SC.WriteLine("Vilken känd regissör gjorde filmen 'The Shining'?");
     break;    }
 
     case (string)"2": {
+        intefall = 2;
          SC.Clear();
         SC.WriteLine(@"Från vilken film är följande citat hämtat?
 
-        I feel the need...the need for speed!");
+I feel the need...the need for speed!");
     break;    }
 
     case (string)"3": {
+        intefall = 2;
         SC.Clear();
         SC.WriteLine("Vad heter den fiktiva staden där TV-serien 'The Simpsons' utspelar sig?");
     break;    }
     
     case (string)"4": {
+        intefall = 2;
         SC.Clear();
         SC.WriteLine("Vilken är den största sjön i Afrika?");
     break;    }
 
     case (string)"5": {
+        intefall = 2;
         SC.Clear();
         SC.WriteLine("Vilket land är världens minsta till ytan?");
     break;    }
 
     case (string)"6": {
+        intefall = 2;
         SC.Clear();
         SC.WriteLine("Vad heter huvudstaden i Australien?");
     break;    }
 
     case (string)"7": {
+        intefall = 2;
          SC.Clear();
         SC.WriteLine(@"Måndagen den 8 december 1980 i New York sköts en man till döds av den förvirrade beundraren Mark Chapman. 
 
-        Vem var det som mördades?");
+Vem var det som mördades?");
     break;    }
 
     case (string)"8": {
+        intefall = 2;
         SC.Clear();
         SC.WriteLine(@"Från vilket land kommer rockgruppen U2?");
     break;    }
 
     case (string)"9": {
+        intefall = 2;
         SC.Clear();
         SC.WriteLine("Vilket år dog Elvis Presley?");
     break;    }
 
     case (string)"11": {
+        intefall = 2;
         SC.Clear();
         SC.WriteLine(Question1);
     break;    }
 
     case (string)"12": {
+        intefall = 2;
         SC.Clear();
         SC.WriteLine(Question2);
     break;    }
